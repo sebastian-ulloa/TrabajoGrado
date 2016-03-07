dfaf3900035fe333b30a170e3dd9887f48b27de1
sebastian29 <sebast_ulloa@hotmail.com>
2016-03-07 13:08:34 -0500

    Deformacion balloon actualizada

--- TrabajoGradoMVC/TrabajoGradoMVC/Modelo/Deformacion.cpp
+++ TrabajoGradoMVC/TrabajoGradoMVC/Modelo/Deformacion.cpp
@@ -1,8 +1,6 @@
 #include "Deformacion.h"
 
-
-Deformacion::Deformacion()
-{
+Deformacion::Deformacion() {
 	esferaSimplex = TMallaSimplex::New();
 	grid = vtkPolyData::New();
 	balloon = TDeformar::New();
@@ -11,12 +9,10 @@ Deformacion::Deformacion()
 	calculadora = Tcalculadora::New();
 }
 
-Deformacion::~Deformacion()
-{
+Deformacion::~Deformacion() {
 }
 
-vtkPolyData* Deformacion::crearEsfera()
-{
+vtkPolyData* Deformacion::crearEsfera() {
 	TEsfera::Pointer esfera = TEsfera::New();
 	TEsfera::VectorType escala;
 	escala.Fill(5.0);
@@ -24,55 +20,36 @@ vtkPolyData* Deformacion::crearEsfera()
 	esfera->SetResolution(5);
 	esfera->Update();
 	convertirSimplex->SetInput(esfera->GetOutput());
+	//convertirSimplex->Update();
 	esferaSimplex = convertirSimplex->GetOutput();
 	grid = conversor.meshToPolydata(esfera->GetOutput());
 	return grid;
 }
 
-TMallaSimplex* Deformacion::triangularASimplex(TMallaTriangular* malla)
-{
+TMallaSimplex* Deformacion::triangularASimplex(TMallaTriangular* malla) {
 	TConvertir::Pointer convertir = TConvertir::New();
 	convertir->SetInput(malla);
 	convertir->Update();
 	return convertir->GetOutput();
 }
 
-TMallaTriangular* Deformacion::simplexATriangular(TMallaSimplex* malla)
-{
+TMallaTriangular* Deformacion::simplexATriangular(TMallaSimplex* malla) {
 	TConvertirContrario::Pointer convertir = TConvertirContrario::New();
 	convertir->SetInput(malla);
 	convertir->Update();
 	return convertir->GetOutput();
 }
 
-void Deformacion::inflar()
-{
-	calculadora->SetSimplexMesh(convertirSimplex->GetOutput());
-	calculadora->Compute();
-	std::cout << "1: " << calculadora->GetVolume();
+void Deformacion::inflar() {
 	balloon->SetInput(esferaSimplex);
 	balloon->SetAlpha(0.2);
 	balloon->SetBeta(0.1);
 	balloon->SetKappa(0.01);
-	balloon->SetIterations(20);
+	balloon->SetIterations(2000);
 	balloon->SetRigidity(0);
 	esferaSimplex = balloon->GetOutput();
 	convertirTriangulo->SetInput(esferaSimplex);
 	convertirTriangulo->Update();
-	calculadora->SetSimplexMesh(esferaSimplex);
-	calculadora->Compute();
-	std::cout << "  -2: " << calculadora->GetVolume();
-	balloon->SetInput(esferaSimplex);
-	balloon->SetAlpha(0.3);
-	balloon->SetBeta(0.1);
-	balloon->SetKappa(0.1);
-	balloon->SetIterations(200);
-	balloon->SetRigidity(0);
-	esferaSimplex = balloon->GetOutput();
-	calculadora->SetSimplexMesh(esferaSimplex);
-	calculadora->Compute();
-	std::cout << "  -3: " << calculadora->GetVolume();
 	grid = conversor.meshToPolydata(convertirTriangulo->GetOutput());
 }
-
 
